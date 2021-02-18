1f9a58205a627b9ba33531aa4533ddb6d67bcc5e
Catherine Blatter <catherine.blatter@unibas.ch>
2021-02-18 18:47:30 +0100

    added code for non-filtering center == 999

--- R/01_prepare.R
+++ R/01_prepare.R
@@ -2,6 +2,14 @@
 # filtered_data with the data for the target center
 # you can pass the argument through the params$center command
 
-filtered_data <- 
-  sample_data %>% 
-  filter(center %in% params$center)
+if (params$center == 999) {
+  # this loop actually does not filter
+  # this allows you to get an 'overall' score
+  # by the fake center-code '999'
+  filtered_data <- sample_data } else {
+    
+    #this loops actually filters the data by its center(s)
+    filtered_data <- 
+      sample_data %>% 
+      filter(center %in% params$center)
+  }
